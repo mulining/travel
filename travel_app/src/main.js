4f79c45768a846991a9ac3dfc50e1045af6e93e1
mishine <2212769640@qq.com>
2019-11-15 20:57:08 +0800

    1

--- travel_app/src/main.js
+++ travel_app/src/main.js
@@ -1,27 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
-import axios from "axios";
-import MintUI from "mint-ui";
-import "mint-ui/lib/style.css";
-import Vant from "vant";
-import "vant/lib/index.css";
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import MintUI from "mint-ui"
+import "mint-ui/lib/style.css"
 
-Vue.use(Vant);
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 //注册Mintui
-Vue.use(MintUI);
+Vue.use(MintUI)
+//注册ElementUI
+Vue.use(ElementUI);
 //axios 注册Vue
-Vue.prototype.$axios = axios;
+Vue.prototype.$axios= axios
 //配置访问服务器的基础路径
-axios.defaults.baseURL = "127.0.0.1:5050";
+axios.defaults.baseURL="127.0.0.1:5050";
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   components: { App },
-  template: "<App/>"
-});
+  template: '<App/>'
+})
--- travel_app/src/router/index.js
+++ travel_app/src/router/index.js
@@ -1,51 +1,15 @@
-import Vue from "vue";
-import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
-import home from "@/views/home";
-import my from "@/views/my";
-import road from "@/views/road";
-import lunBo from "@/components/lunBo";
-import tabBar from "@/components/tabBar";
-import search from "@/components/search";
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "HelloWorld",
+      path: '/',
+      name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: "/lunBo",
-      name: "lunBo",
-      component: lunBo
-    },
-    {
-      path: "/my",
-      name: "my",
-      component: my
-    },
-    {
-      path: "/search",
-      name: "search",
-      component: search
-    },
-    {
-      path: "/tabBar",
-      name: "tabBar",
-      component: tabBar
-    },
-    {
-      path: "/road",
-      name: "road",
-      component: road
-    },
-    {
-      path: "/home",
-      name: "home",
-      component: home
     }
   ]
-});
+})
